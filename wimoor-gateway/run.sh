b87999e2e4134d7ba9b60c1baa1cac6e29eca8d5
alexwang <895706822@qq.com>
2025-01-08 13:56:52 +0800

    修改启动脚本

--- wimoor-gateway/run.sh
+++ wimoor-gateway/run.sh
@@ -4,6 +4,6 @@ VERSION=v1
 if [ -n "$1" ] ;then
   VERSION="$1"
 fi
-sed -i "s|aolifu/erp-boot:.*|aolifu/erp-boot:$VERSION|g" ./docker-compose.yml
+sed -i "s|aolifu/wimoor-gateway:.*|aolifu/wimoor-gateway:$VERSION|g" ./docker-compose.yml
 docker-compose up -d
 
