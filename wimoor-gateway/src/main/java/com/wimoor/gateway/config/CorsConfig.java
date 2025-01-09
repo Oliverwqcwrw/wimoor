c850a5670c88449c6825b4713ab258b3bd1f4db8
alexwang <895706822@qq.com>
2025-01-09 14:47:30 +0800

    升级jar

---
+++ wimoor-gateway/src/main/java/com/wimoor/gateway/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.wimoor.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @package: com.wimoor.gateway.config
+ * @className: CorsConfig
+ * @author: alexwang
+ * @description: TODO
+ * @date: 2025/1/9 14:41
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://216.250.97.34","http://localhost") // 允许来自http://localhost的请求
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的HTTP方法
+                .allowedHeaders("*"); // 允许的HTTP头部
+    }
+}
